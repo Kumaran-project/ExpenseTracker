6da7d85f549ffa4fee0c613a8a1cfb39f8cc581c
Kumaran-project <kumaranselvaraj08@gmail.com>
2024-11-15 21:53:51 +0530

    encrypted and handled the logic

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,39 +1,43 @@
-  const Sequelize=require("sequelize")
-const user=require("../models/user")
-module.exports.postUser=(req,res)=>{
-  console.log(req.body);
-  
-   user.create(req.body).then((result)=>{
-    res.status(201).json(result);
-   }).catch((error)=>{
-    if (error instanceof Sequelize.UniqueConstraintError) {
-      return res.status(409).json({ error: 'Email is already registered' });
+const Sequelize = require("sequelize");
+const bcrypt = require("bcrypt");
+const user = require("../models/user");
+module.exports.postUser = async (req, res) => {
+  const { user_name, password, email } = req.body;
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await user.create({ user_name, email, password: hashedPassword });
+    res.status(201).json(newUser);
+  } catch (error) {
+    if (error.name === "SequelizeUniqueConstraintError") {
+      return res.status(409).json({ success: false, message: "Email already registered" });
     }
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
+};
+
+module.exports.loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const existingUser = await user.findOne({ where: { email: email } });
+    if (!existingUser) {
+      res.status(404).json({ success: false, message: "404, No user found" });
+    } else {
+      bcrypt.compare(password, existingUser.password).then((result) => {
+        if (result) {
+          res
+            .status(200)
+            .json({ success: true, message: "User logged in successfully" });
+        } else {
+          res
+            .status(401)
+            .json({
+              success: true,
+              message: "Authentication failed, Invalid password",
+            });
+        }
+      });
+    }
+  } catch (error) {
     console.log(error);
-   })
-}
-
-
-module.exports.loginUser=async(req,res)=>{
-  const {email,password}=req.body;
-  try{
-  const existingUser= await user.findOne({where:{email:email}});
-  if(!existingUser)
-  {
-    res.status(404).json({success:false,message:"404, No user found"});
   }
-  else{
-    const user_password=existingUser.password;
-    if(user_password===password){
-      res.status(200).json({success:true,message:"User logged in successfully"});
-    }
-    else{
-      res.status(401).json({success:true,message:"Authentication failed, Invalid password"});
-    }
-  }
-  }
-  catch(error){
-    console.log(error);
-  }
-}
-
+};
